b4ae0556cc5f2bb01aed526a599aeffea4f2094e
unknown <terrychen8641@gmail.com>
2022-11-01 18:31:00 +0800

    add find file

--- products.py
+++ products.py
@@ -1,16 +1,24 @@
-#讀取檔案
+import os # operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #跳到下一個迴圈的意思，break與continue都一定在迴圈內
-		name, price = line.strip().split(',')
-		products.append([name, price])
+if os.path.isfile('products.csv'): #尋找並讀取檔案
+	print('yeah! 找到檔案了!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #跳到下一個迴圈的意思，break與continue都一定在迴圈內
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案.....')
 
-print(products)
 
 
 
+
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q': #quit
