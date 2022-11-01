491a936623b9e7216b6e794e5be6114d8fffb708
unknown <terrychen8641@gmail.com>
2022-11-01 18:18:10 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,16 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #跳到下一個迴圈的意思，break與continue都一定在迴圈內
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+print(products)
+
+
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q': #quit
@@ -15,17 +27,18 @@ while True:
 #	products.append(p)
 #法三
 	products.append([name, price])
-print(products)
 
+#印出所有購買紀錄
 for p in products:
-	print(p)
 	print(p[0], '的價格是', p[1])
 
-#'abc' + '123' = 'abc123'
-#'abc' * '123' = 'abcabcabc'
-#字串不可以做減跟除，乘法也很少做
+# Note:
+# 'abc' + '123' = 'abc123'
+# 'abc' * '123' = 'abcabcabc'
+# 字串不可以做減跟除，乘法也很少做
 
-with open('products.csv', 'w', encoding='UTF-8') as f: #or 'products.txt', encoding='utf-8'避免亂碼問題
+#寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f: #or 'products.txt', encoding='utf-8'避免亂碼問題
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
